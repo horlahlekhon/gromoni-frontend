332d13b399d43bcaf37ad37fed6c1dd2dc2a426f
Philippa Olomoro <olomoro.philippa@gmail.com>
2020-09-29 01:12:41 +0100

    third commit

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const Root = (props) => {
               <Route exact path={`${process.env.PUBLIC_URL}/`} component={CommingSoon} />
               <Route exact path={`${process.env.PUBLIC_URL}/login`} component={SignInAndRegister} />
               {/* <Route exact path={`${process.env.PUBLIC_URL}/signup`} component={Register} /> */}
-              <Route exact path={`${process.env.PUBLIC_URL}/user/business/`} component={NewBusiness} />
+              {/* <Route exact path={`${process.env.PUBLIC_URL}/user/business/`} component={NewBusiness} /> */}
               <Route path="/welcomepage"><WelcomePage/></Route>
 
               <Fragment>
