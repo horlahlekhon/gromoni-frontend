c199b70cb0180096a0078167505745e1c435e598
ai readone <ridwanibrahim97@hotmail.com>
2020-10-02 11:06:54 +0100

    Update SalesCashBalance.jsx

--- src/components/home/SalesCashBalance.jsx
+++ src/components/home/SalesCashBalance.jsx
@@ -9,34 +9,37 @@ const SalesCashBalance = (props) => {
 
 	return(
 			 <Fragment>
-            <Container fluid={true}>
-                <Row>
-                    <Col md="12" sm="12">
-                        <Card>
-                            <CardHeader className="b-l-primary">
-                                <h2> Balance Overview</h2>
-                            </CardHeader>
-                            <CardBody>
-                               <Row>
-                               		<Col xs="12" md="4">
-                               			<h5> Total Sales Cash </h5>
-                               			<p> #3,459 909.2124 </h5>
-                               		</Col>
+	            <Container fluid={true}>
+	                <Row>
+	                    <Col md="12" sm="12">
+	                        <Card>
+	                            <CardHeader className="b-l-primary">
+	                                <h2> Balance Overview</h2>
+	                            </CardHeader>
+	                            <CardBody>
+	                               <Row>
+	                               		<Col xs="12" md="4">
+	                               			<h5> Total Sales Cash </h5>
+	                               			<p> #3,459 909.2124 </p>
+	                               		</Col>
 
-                               		<Col xs="12" md="4">
-                               			<h5> Recieved Cash </h5>
-                               			<p> #2,567 900.000 </h5>
-                               		</Col>
+	                               		<Col xs="12" md="4">
+	                               			<h5> Recieved Cash </h5>
+	                               			<p> #2,567 900.000 </p>
+	                               		</Col>
 
-                               		<Col xs="12" md="4">
-                               			<h5> Recievable Cash </h5>
-                               			<p> #959 123.2124 </h5>
-                               		</Col>
-                               </Row>
-                            </CardBody>
-                        </Card>
-                    </Col>
+	                               		<Col xs="12" md="4">
+	                               			<h5> Recievable Cash </h5>
+	                               			<p> #959 123.2124 </p>
+	                               		</Col>
+	                               </Row>
+	                            </CardBody>
+	                        </Card>
+	                    </Col>
+	                </Row>
+	            </Container>
+	         </Fragment>
 	)
 }
 
-export default SalesCashBalance;s
+export default SalesCashBalance;
