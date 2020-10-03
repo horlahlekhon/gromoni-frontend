c6d24216d541752d433c5e25f2374746a2aabb2c
ai readone <ridwanibrahim97@hotmail.com>
2020-10-03 07:26:11 +0100

    Update CreateNewButtons.jsx

--- src/components/home/CreateNewButtons.jsx
+++ src/components/home/CreateNewButtons.jsx
@@ -1,8 +1,7 @@
 //create new sales Button, NEW PRODUCTS BUTTON, NEW CUSTOMER BUTTON
 
 import React, {Fragment,useState} from 'react';
-import {Container,Row,Col,Card,CardHeader,CardBody,Button, ButtonGroup} from 'reactstrap';
-
+import {Container,Row,Col,Card,CardHeader,CardBody,Button, ButtonGroup,FatAdd, Cart, Users} from 'reactstrap';
 
 const CreateNewButtons = (props) => {
 	
@@ -11,28 +10,28 @@ const CreateNewButtons = (props) => {
 		<Fragment>
 			<Container fluid={true}>
 	      <Row>
-	      	
-	  		<Button className="col md-4 sm-12 btn-square btn-lg btn-air-light " color="light" style={{height:'50px'}}>
-	  	 
-	  			
-	  				Light Button
-	  			
-	  		</Button>
-	  	
+	      	<Col md="4" sm="12">
+	  			<Button className="col  btn-square btn-air-light" size="lg" block color="light" >
+	  				<FatAdd />
+  					Create New Sales
+	  				}
+	  			</Button>
+	  		</Col>
 
-	  	
-	  		<Button className="col md-4 sm-12 btn-square btn-lg btn-air-light " color="light">
-	  	 
-	  			Light Button
-	  		</Button>
-	  	
+	  		<Col md="4" sm="12">
+	  			<Button className="col  btn-square btn-air-light" size="lg" block color="light" >
+	  				<User />
+  					Create New Customer
+	  			</Button>
+	  		</Col>
 
+	  		<Col md="4" sm="12">
+	  			<Button className="col  btn-square btn-air-light" size="lg" block color="light" >
+  					<Cart />
+  					Create New Product
+	  			</Button>
+	  		</Col>
 	  	
-	  		<Button className="col md-4 sm-12 btn-square btn-lg btn-air-light" size="lg" color="light">
-	  	 
-	  			Light Button
-	  		</Button>
-	      	
 	      </Row> 
 	    	</Container>         
 		</Fragment>
