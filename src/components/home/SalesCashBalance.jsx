104d4f08f6a6b133cc0cb452bfd0a6bdfb631286
ai readone <ridwanibrahim97@hotmail.com>
2020-11-22 14:59:15 +0100

    home page complete
    build ran succesfully
    
    but the chart is yet to be fixed

--- src/components/home/SalesCashBalance.jsx
+++ src/components/home/SalesCashBalance.jsx
@@ -1,17 +1,92 @@
 // this component displays trade BALACES
-// such as account TOTAL SALES AMOUNT, AMOUNT RECIEVED, AMOUNT RECIEVABLE
+// such as account TOTAL SALES AMOUNT, AMOUNT RECIEVED, AMOUNT RECIEVABLE, product sold
 
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import {Container,Row,Col,Card,CardBody} from "reactstrap";
 
 const SalesCashBalance = (props) => {
 
 
-    return (
-        <div>
-            <h1> This is sales cash balance</h1>
-        </div>
-    )
+	return(
+			 <Fragment  >
+	            <Container fluid={true} >
+	                <Row >
+
+	                    <Col xl="12" className="xl-100 box-col-12">
+							
+							    <Row>
+					              <Col className="d-xl-none sm-hidden" sm="2">
+					              </Col>
+
+
+									<Col xl="4" sm="8">
+	  					                <Card className="o-hidden text-center">
+								  			<CardBody className="crypto-current">
+										        <div className="current-balance">
+										        	<span className="f-12 f-w-600">Total Sales Cash</span>
+										          <h2>
+										          	<span className="mr-2">
+										          		<i className="icofont icofont-pound"></i>
+										          	</span>
+										          	<p>3,459 909.2124</p>
+										          </h2>
+										          
+										        </div>
+										 	</CardBody>
+										</Card>
+									</Col>
+
+									<Col className="d-xl-none sm-hidden" sm="2">
+					                </Col>
+
+
+									<Col className="d-xl-none sm-hidden" sm="2">
+					                </Col>
+					                
+									<Col xl="4" sm="8">
+	  					                <Card className="o-hidden text-center">
+								  			<CardBody className="crypto-current">
+										        <div className="current-balance">
+										        	<span className="f-12 f-w-600">Product Sold</span>
+										          <h2>
+										          	<span className="mr-2">
+										          		<i className="icofont icofont-pound"></i>
+										          	</span>
+										          	<p>3,459 909.2124</p>
+										          </h2>
+										          
+										        </div>
+										 	</CardBody>
+										</Card>
+									</Col>
+
+									<Col className="d-xl-none sm-hidden" sm="2">
+					                </Col>
+
+									<Col xl="4" sm="8">
+	  					                <Card className="o-hidden text-center">
+								  			<CardBody className="crypto-current">
+										        <div className="current-balance text-center">
+										        	<span className="f-12 f-w-600">Debt</span>
+										          <h2>
+										          	<span className="mr-2">
+										          		<i className="icofont icofont-pound"></i>
+										          	</span>
+										          	<p>3,459 909.2124</p>
+										          </h2>
+										          
+										        </div>
+										 	</CardBody>
+										</Card>
+									</Col>
+
+							    </Row>
+							
+						</Col>
+	                </Row>
+	            </Container>
+	         </Fragment>
+	)
 }
 
 export default SalesCashBalance;
--- src/components/home/SalesCashBalanceChart.jsx
+++ src/components/home/SalesCashBalanceChart.jsx
@@ -1,16 +1,71 @@
-// SALES CASH BALANCE VIEW CHART
+// SALES CASH BALANCE VIEW CHART 
+import React, { Fragment, useState, useEffect} from 'react';
+import Chart from 'react-apexcharts';
+import {Container,  Row, Col,TabPane,TabContent,Button,ButtonGroup} from 'reactstrap';
 
-import React from 'react';
+import {HomeCashBalanceChartOptions} from "./homeUtility";
 
 
-const SalesCashBalanceChart = (props) => {
+ const SalesCashBalanceChart = (props) => {
+	// THIS IS USE  TOSWITCH BETWEEN WEEEKLY, MONTHLY AND YEARLY CHARTS
+	const [activeTab, setActiveTab] = useState('1');
 
+    const weeklyCashBalanceChart = HomeCashBalanceChartOptions(props.weeklyCashBalanceChartData.series,
+        props.weeklyCashBalanceChartData.labels);
+    
+    const monthlyCashBalanceChart = HomeCashBalanceChartOptions(props.monthlyCashBalanceChartData.series,
+        props.monthlyCashBalanceChartData.labels);
 
-    return (
-        <div>
-            <h1> This is sales cash balance Chart</h1>
-        </div>
-    )
-}
+    const yearlyCashBalanceChart = HomeCashBalanceChartOptions(props.yearlyCashBalanceChartData.series,
+        props.yearlyCashBalanceChartData.labels)
+
+        useEffect(() => console.log(props.monthlyCashBalanceChartData.series), [props.monthlyCashBalanceChartData.series])
+
+        // console.log(monthlyCashBalanceChart.series)
+
+ return (
+        <Fragment >
+      
+            <Container fluid={true} >
+            
+                <Row >
+                    <Col lg="12 box-col-12" xl="6 xl-100 ">
+                        <div className="text-center">
+                            <ButtonGroup >
+                                <Button className={ activeTab === '1' ? 'active' : ''} onClick={() => setActiveTab('1')} color="light" >Weekly</Button>
+                                <Button className={activeTab === '2' ? 'active' : ''} onClick={() => setActiveTab('2')} color="light">Monthly</Button>
+                                <Button className={activeTab === '3' ? 'active' : ''} onClick={() => setActiveTab('3')} color="light">Yearly</Button>
+                            </ButtonGroup>
+                        </div>
+                        <div className="tabbed-card">
+                               
+                                <TabContent activeTab={activeTab}>
+                                    <TabPane tabId="1">
+                                        <div id="column-chart">
+						               		 <Chart options={weeklyCashBalanceChart.options} series={weeklyCashBalanceChart.series} height={400} type="bar" />
+						                </div>
+                                    </TabPane>
+                                    <TabPane tabId="2">
+                                        <div id="column-chart">
+						               		 <Chart options={monthlyCashBalanceChart.options} series={monthlyCashBalanceChart.series} height={400} type="bar" />
+						                </div>
+                                    </TabPane>
+                                    <TabPane tabId="3">
+                                        <div id="column-chart">
+						               		 <Chart options={yearlyCashBalanceChart.options} series={yearlyCashBalanceChart.series} height={400} type="bar" />
+						                </div>
+                                    </TabPane>
+                                </TabContent>
+                            </div>
+                          
+                    </Col>
+                    
+                </Row>
+            
+            </Container>
+        </Fragment>
+    );
+};
+
 
 export default SalesCashBalanceChart;
